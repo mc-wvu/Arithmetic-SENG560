c6f7616b4f05a8c493c33ba9b01f65e42c0acc41
Ryan Barth <ryanbindc@gmail.com>
2023-09-19 16:15:34 -0400

    Initial Commit

---
+++ src/ArithmeticTester.java
@@ -0,0 +1,8 @@
+import arithmetic.Arithmetic;
+
+public class ArithmeticTester {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic();
+        System.out.println(arithmetic.addHex("8AB", "B78"));
+    }
+}
